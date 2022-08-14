e9bae5bebf9fcd6c76b49d9c2e6ef87dbec84153
silvajpedro <belojpedro.31@gmail.com>
2022-08-13 22:01:28 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import Movies from './Movies/Movies.js'
+export default class App extends Component{
+  render(){
+    return(
+      <div>
+<Movies/>
+      </div>
+    )
+  }
+}
